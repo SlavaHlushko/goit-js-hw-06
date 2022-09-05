af2a9aeb106be837e2c626c491333aa4f280c8ae
SlavaHlushko <slavahlushko90@gmail.com>
2022-09-05 23:50:19 +0300

    add

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const itemsEl = document.querySelectorAll('.item');
+console.log(`Number of categories:${itemsEl.length}`);
+
+itemsEl.forEach(item => {
+    const titleEl = item.querySelector('h2');
+    const quantityItem = item.querySelectorAll('li');
+    console.log(`Category: ${titleEl.textContent}`);
+    console.log(`Elements: ${quantityItem.length}`);
+}
+);
