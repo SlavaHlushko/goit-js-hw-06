85e9d6adaec0963d2588cdfd45ce3612ca936d23
SlavaHlushko <slavahlushko90@gmail.com>
2022-09-10 18:28:25 +0300

    Update task 9-js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+'use strict';
+
+const refs = {
+  colorButton: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+  body: document.querySelector('body'),
+};
+refs.colorButton.addEventListener('click', changeColorButton);
+
+function changeColorButton() {
+  const currentColor = getRandomHexColor();
+  refs.color.textContent = currentColor;
+  refs.body.style.background = currentColor;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
